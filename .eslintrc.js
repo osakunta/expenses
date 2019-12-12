6db40ed690537bab03d3899ac5780343e3cc99f9
vkopio <ville.kopio@gmail.com>
2019-12-12 16:06:35 +0200

    Change config to allow absolute imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,4 +25,11 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': 0,
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
 };
