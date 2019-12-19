4014a6adbb99ea182af1dfded43f49140ff32106
Ville Kopio <ville.kopio@kodan.fi>
2019-12-19 15:28:12 +0200

    Added pdf-lib for pdf merging.
    Changed attachments to be merged with pdf-lib. This is a bit more
    complicated than before but it makes it possible to merge pdf
    documents.

--- src/utils/generate-pdf.js
+++ src/utils/generate-pdf.js
@@ -1,4 +1,15 @@
 import * as JsPdf from 'jspdf';
+import { PDFDocument } from 'pdf-lib';
+
+const savePdf = async (pdf, fileName) => {
+  const pdfBytes = await pdf.save();
+  const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+  const link = document.createElement('a');
+
+  link.href = window.URL.createObjectURL(blob);
+  link.download = fileName;
+  link.click();
+};
 
 const readFile = (file) => {
   const reader = new FileReader();
@@ -32,7 +43,8 @@ const boldText = (doc, string, x, y, options) => {
   doc.setFontType('normal');
 };
 
-const generateBill = (doc, bill, date) => {
+const generateBill = (bill, date) => {
+  const doc = new JsPdf();
   const splitDescription = doc.splitTextToSize(bill.expensesDescription, 280);
   const totalOffset = 7 * (bill.expenses.length + 1);
 
@@ -91,28 +103,38 @@ const generateBill = (doc, bill, date) => {
   doc.rect(105, 270, 90, 12);
   doc.text('Yhteensä EUR:', 105 + 2, 275);
   doc.text(bill.expensesTotal, 105 + 2, 280);
+
+  return doc.output('arraybuffer');
+};
+
+const generateImagePage = async (pdfDoc, attachment) => {
+  try {
+    const doc = new JsPdf();
+    const image = await readFile(attachment);
+    const ratio = image.height / image.width;
+
+    doc.addImage(image, 'JPEG', 15, 15, 180, 180 * ratio);
+
+    const attachmentDoc = await PDFDocument.load(doc.output('arraybuffer'));
+    const [attachmentPage] = await pdfDoc.copyPages(attachmentDoc, [0]);
+
+    pdfDoc.addPage(attachmentPage);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const generatePdf = async (bill) => {
-  const doc = new JsPdf();
   const date = new Date();
-
-  generateBill(doc, bill, date);
+  const fileName = `lasku_${date.getFullYear()}-${date.getMonth()}-${date.getDate()}.pdf`;
+  const pdfDoc = await PDFDocument.load(generateBill(bill, date));
 
   const generatedAttachments = bill.attachments.map(async (attachment) => {
-    try {
-      const image = await readFile(attachment);
-      const ratio = image.height / image.width;
-
-      doc.addPage();
-      doc.addImage(image, 'JPEG', 15, 15, 180, 180 * ratio);
-    } catch (error) {
-      console.error(error);
-    }
+    await generateImagePage(pdfDoc, attachment);
   });
 
   Promise.all(generatedAttachments)
-    .then(() => doc.save(`lasku_${date.getFullYear()}-${date.getMonth()}-${date.getDate()}.pdf`))
+    .then(() => savePdf(pdfDoc, fileName))
     .catch((error) => console.error(error));
 };
 
